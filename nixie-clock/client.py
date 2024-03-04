a7d3df046f503e07fc23968e4a624354822f31f7
Anmol Parande <parandea1.7@gmail.com>
2024-03-03 22:59:07 -0800

    Add nixie test client function

--- nixie-clock/client.py
+++ nixie-clock/client.py
@@ -1,25 +1,58 @@
+import argparse
 import asyncio
+import contextlib
 from datetime import datetime
+from typing import AsyncGenerator
 
 import bleak
 
 CHAR_UID = '19B10001-E8F2-537E-4F6C-D104768A1215'
 
 
-async def main():
+@contextlib.asynccontextmanager
+async def _connect() -> AsyncGenerator[bleak.BleakClient, None]:
     arduino = await bleak.BleakScanner.find_device_by_name('Nixie-Clock')
     if not arduino:
         print('Could not connect to Arduino')
         return
 
     async with bleak.BleakClient(arduino) as arduino_client:
-        now = datetime.now()
-        hour_bytes = now.hour.to_bytes(1, 'little')
-        minute_bytes = now.minute.to_bytes(1, 'little')
-        await arduino_client.write_gatt_char(CHAR_UID,
-                                             hour_bytes + minute_bytes,
-                                             response=True)
+        yield arduino_client
 
+async def _set_time(arduino_client: bleak.BleakClient, hour: int, minute: int):
+    hour_bytes = hour.to_bytes(1, 'little')
+    minute_bytes = minute.to_bytes(1, 'little')
+    await arduino_client.write_gatt_char(CHAR_UID,
+                                         hour_bytes + minute_bytes,
+                                         response=True)
+
+
+async def set_time():
+    async with _connect() as arduino_client:
+        now = datetime.now()
+        await _set_time(arduino_client, now.hour, now.minute)
+
+
+async def test_nixies():
+    async with _connect() as arduino_client:
+        for i in range(10):
+            hour = 10 * i + i
+            minute = 10 * i + i
+            await _set_time(arduino_client, hour, minute)
+            await asyncio.sleep(10)
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group(required=True)
+
+    group.add_argument('--set-time', action='store_true', help="Set the time")
+    group.add_argument('--test-nixies', action='store_true', help="Test each digit of each tube")
+
+    args = parser.parse_args()
+    
+    if args.set_time:
+        asyncio.run(set_time())
+    elif args.test_nixies:
+        asyncio.run(test_nixies())
+    else:
+        assert False, "Impossible!"
