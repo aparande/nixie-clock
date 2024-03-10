3613793695642d7a597277f3fa5ab18a8953508e
Anmol Parande <parandea1.7@gmail.com>
2024-03-09 21:54:20 -0800

    Add dwell time argument to testing the tubes

--- nixie-clock/client.py
+++ nixie-clock/client.py
@@ -19,6 +19,7 @@ async def _connect() -> AsyncGenerator[bleak.BleakClient, None]:
     async with bleak.BleakClient(arduino) as arduino_client:
         yield arduino_client
 
+
 async def _set_time(arduino_client: bleak.BleakClient, hour: int, minute: int):
     hour_bytes = hour.to_bytes(1, 'little')
     minute_bytes = minute.to_bytes(1, 'little')
@@ -33,26 +34,33 @@ async def set_time():
         await _set_time(arduino_client, now.hour, now.minute)
 
 
-async def test_nixies():
+async def test_nixies(dwell_time: float):
     async with _connect() as arduino_client:
         for i in range(10):
             hour = 10 * i + i
             minute = 10 * i + i
             await _set_time(arduino_client, hour, minute)
-            await asyncio.sleep(10)
+            await asyncio.sleep(dwell_time)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--dwell-time',
+                        type=float,
+                        default=10,
+                        help='How long to spend on each digit')
     group = parser.add_mutually_exclusive_group(required=True)
 
     group.add_argument('--set-time', action='store_true', help="Set the time")
-    group.add_argument('--test-nixies', action='store_true', help="Test each digit of each tube")
+    group.add_argument('--test-nixies',
+                       action='store_true',
+                       help="Test each digit of each tube")
 
     args = parser.parse_args()
-    
+
     if args.set_time:
         asyncio.run(set_time())
     elif args.test_nixies:
-        asyncio.run(test_nixies())
+        asyncio.run(test_nixies(args.dwell_time))
     else:
         assert False, "Impossible!"
